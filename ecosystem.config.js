69b632dee76429555ce8040bae3c0014575a19a6
Lewis Lopez <lopezsoft.com@gmail.com>
2025-06-14 00:22:32 -0500

    v1.6.2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = [{
   script: 'dist/server.js',
-  name: 'multipremium-back',
-  exec_mode: 'fork',
-  cron_restart: '05 00 * * *',
-  max_memory_restart: '769M', // Configuração para reiniciar quando atingir 769 MB de memória
-  node_args: '--max-old-space-size=769', // Limite de memória do Node.js para 769 MB
-  watch: false
+  name: 'chatikpro-back',
+  exec_mode: 'cluster',            // ✅ Modo de ejecución en clúster
+  instances: 'max',                // ✅ Usar el máximo número de instancias disponibles
+  max_memory_restart: '2G',        // ✅ Reinicio si la memoria supera 2 GB
+  node_args: '--max-old-space-size=2048', // ✅ Argumentos de Node.js para limitar el espacio de memoria
+  cron_restart: '05 00 * * *',     // ✅ Reinicio programado a las 00:05 todos los días
+  watch: false                     // ✅ Desactivado el modo de vigilancia
 }]
